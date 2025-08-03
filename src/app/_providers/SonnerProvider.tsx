1273e7bbfc7660bbd82ee9e8f2ab239344139103
Sangmin Park <sakedon2151@gmail.com>
2025-08-04 01:35:14 +0900

    feat/#10 sonner provider 추가

---
+++ src/app/_providers/SonnerProvider.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { Toaster } from '@/shared/ui/Sonner';
+
+export const SonnerProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      {children}
+      <Toaster position='top-center' richColors />
+    </>
+  );
+};
