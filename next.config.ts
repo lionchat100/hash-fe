44945d42645fa0d8c87d0fc69a454e3aaf6ada35
Sangmin Park <sakedon2151@gmail.com>
2025-08-05 00:39:22 +0900

    fix/#11 next config 재구성

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,11 @@ import type { NextConfig } from 'next';
 import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: false,
-    sw: '/worker.js',
-    // 추후 캐싱 전략 수립하기
-  },
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  sw: '/worker.js',
+  disable: process.env.NODE_ENV === 'development',
 });
 
 export default nextConfig;
