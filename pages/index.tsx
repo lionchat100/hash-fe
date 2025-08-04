34bcca43b207d763d5f4b6274a4ec2978b1928b1
Sangmin Park <sakedon2151@gmail.com>
2025-08-04 20:30:23 +0900

    fix/#11 기획 변경에 따른 서비스 명칭 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,21 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 const LandingPage = () => {
   return (
     <>
       <Head>
-        <title>Hash</title>
-        <meta name="description" content="Hash" />
-        <meta property="og:title" content="Hash - 새로운 인연을 만나보세요" />
-        <meta property="og:description" content="개발자 커피챗 플랫폼 Hash" />
-        <meta property="og:type" content="website" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>LIONCHAT</title>
+        <meta name='description' content='LIONCHAT' />
+        <meta property='og:title' content='LIONCHAT - 새로운 만남을 시작하다' />
+        <meta property='og:description' content='개발자 커피챗 플랫폼 LIONCHAT' />
+        <meta property='og:type' content='website' />
       </Head>
 
-      <main className="">
-        <div className="">
-          <h1>Hash</h1>
-          <p>개발자 커피챗 플랫폼 해쉬에서 새로운 인연을 만나보세요!</p>
-          <a href="/login">시작하기</a>
+      <main className=''>
+        <div className=''>
+          <h1>LIONCHAT</h1>
+          <p>개발자 커피챗 플랫폼 LIONCHAT에서 새로운 만남을 시작하세요!</p>
+          <a href='/login'>시작하기</a>
         </div>
       </main>
     </>
