530dd44f7486d769df81d3f0f21d58fd18efe701
Sangmin Park <sakedon2151@gmail.com>
2025-08-09 20:19:00 +0900

    fix/#35 쿠키 credentials 속성 추가 및 토큰 관련 테스트를 위한 로직 주석 처리

--- src/features/update-user/model/userOAuthLogin.ts
+++ src/features/update-user/model/userOAuthLogin.ts
@@ -14,16 +14,17 @@ export const userOAuthLogin = async (code: string) => {
       localStorage.setItem('accessToken', accessToken);
     }
 
-    const refreshToken = getCookie('refresh_token');
-    if (refreshToken) {
-      setCookie('refresh_token', refreshToken, {
-        httpOnly: false,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
-        maxAge: 7 * 24 * 60 * 60, // 7일
-        path: '/',
-      });
-    }
+    // comment: 새로고침으로 인한 토큰 휘발시 주석 해제
+    // const refreshToken = getCookie('refresh_token');
+    // if (refreshToken) {
+    //   setCookie('refresh_token', refreshToken, {
+    //     httpOnly: false,
+    //     secure: process.env.NODE_ENV === 'production',
+    //     sameSite: 'lax',
+    //     maxAge: 7 * 24 * 60 * 60, // 7일
+    //     path: '/',
+    //   });
+    // }
 
     const currentUser = await getCurrentUser();
 
