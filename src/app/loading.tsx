0433ad61c2816a0b0b9ab515b05656d343809a26
haSong <70552536+poan1221@users.noreply.github.com>
2025-08-12 16:39:21 +0900

    feat/#42 이미지 구역 확인

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ export default function LoadingPage() {
       <div className="rounded-40 h-(--space-h-btn-lg) bg-stone-800 px-6 py-4 text-base font-medium text-stone-50">
         페이지로 진입하고 있어요!
       </div>
-      <div className="flex h-48 w-48 items-center justify-center">토킷 이미지</div>
+      <div className="flex h-48 w-48 items-center justify-center">임시 토킷 이미지 구역</div>
       <div className="font-display-sm pb-14 text-stone-900">
         <p>로딩중이에요</p>
         <p>조금만 기다려주세요</p>
