5d3acc7f896346241d6642661f0dbe71cc5aea06
haSong <70552536+poan1221@users.noreply.github.com>
2025-08-20 20:33:18 +0900

    feat/#79 피드, 댓글 프로필 링크 연결 및 스타일 깨짐 수정

--- src/features/update-comment/ui/CommentCard.tsx
+++ src/features/update-comment/ui/CommentCard.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { CommentItem } from '@/entities/comment/model/types';
 import { useUserStore } from '@/entities/user';
 import { formatRelativeTime } from '@/shared/lib/dateUtils';
@@ -14,6 +15,7 @@ interface Props {
 }
 
 export const CommentCard = ({ item, onDeleted }: Props) => {
+  const route = useRouter();
   const [showWarningModal, setShowWarningModal] = useState(false);
 
   const { currentUser } = useUserStore();
@@ -41,13 +43,18 @@ export const CommentCard = ({ item, onDeleted }: Props) => {
     <div className="flex flex-col items-start gap-0.5">
       <div className="flex w-full justify-between">
         <div className="flex items-center gap-2">
-          <div className="flex-shrink-0">
-            <Avatar className="size-7">
-              <AvatarImage src={item.writer.imageUrl} alt={item.writer.nickname} />
-              <AvatarFallback className="text-xs">{item.writer.nickname.charAt(0)}</AvatarFallback>
-            </Avatar>
+          <div
+            className="flex cursor-pointer items-center gap-2"
+            onClick={() => route.push(`/profile/${item.writer.id}`)}
+          >
+            <div className="flex-shrink-0">
+              <Avatar className="size-7">
+                <AvatarImage src={item.writer.imageUrl} alt={item.writer.nickname} />
+                <AvatarFallback className="text-xs">{item.writer.nickname.charAt(0)}</AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="text-sm font-medium text-stone-900">{item.writer.nickname}</div>
           </div>
-          <div className="text-sm font-medium text-stone-900">{item.writer.nickname}</div>
           <div className="text-xs text-stone-700">{formatRelativeTime(item.createdAt)}</div>
         </div>
         {isMyComment && (
