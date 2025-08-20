dbb1e6434aaf58732663d549e28472575c431195
Sangmin Park <sakedon2151@gmail.com>
2025-08-20 23:04:28 +0900

    feat/#87 채팅 시작 기능 간단 구현

--- src/features/update-chat/index.ts
+++ src/features/update-chat/index.ts
@@ -1,5 +1,9 @@
+// API
+export { initChatRoom } from './api/initChatRoom';
+
 // Model
 export { useChatSubscription } from './model/chatSubscription';
+export { useChatStartOnExplore } from './model/chatStartOnExplore';
 
 // Lib
 export { pushLiveMessage } from './lib/pushLiveMessage';
