41b4c7e593aa496f3843e1c8aa9c386a503db243
Sangmin Park <sakedon2151@gmail.com>
2025-08-22 07:09:51 +0900

    feat/#103 좋아요한 프로필 리스트 쿼리 추가

--- src/entities/user/api/getLikeProfiles.ts
+++ src/entities/user/api/getLikeProfiles.ts
@@ -1,7 +1,19 @@
 import api from '@/shared/api/axios';
 import { UserProfile } from '../model/types';
+import { useQuery } from '@tanstack/react-query';
 
 export const getLikeProfiles = async () => {
   const response = await api.get<UserProfile[]>('/users/likes/lists');
   return response.data;
+};
+
+export const useLikeProfilesQuery = () => {
+  return useQuery({
+    queryKey: ['likeProfiles'],
+    queryFn: getLikeProfiles,
+    staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 30,
+    refetchOnWindowFocus: true,
+    refetchOnMount: true,
+  });
 };
