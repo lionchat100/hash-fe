9bbde338bdc64f3c259af7964f1572b38548b6d2
Sangmin Park <sakedon2151@gmail.com>
2025-08-22 06:55:39 +0900

    fix/#103 일관성있는 헤더 레이아웃으로 변경

--- src/entities/user/ui/LikeProfilesHeader.tsx
+++ src/entities/user/ui/LikeProfilesHeader.tsx
@@ -1,21 +1,16 @@
 import { Button } from '@/shared/ui/Button';
-import { ChevronLeft, Siren } from 'lucide-react';
+import { ChevronLeft } from 'lucide-react';
 import Link from 'next/link';
 
 export const LikeProfilesHeader = () => {
   return (
-    <div className="">
-      <div className="flex items-center justify-between bg-white">
+    <header className="safe-pt sticky top-0 z-50 flex h-(--space-h-header) w-full items-center justify-center bg-white">
+      <Button variant="ghost" size="icon" className="absolute top-4 left-4" asChild>
         <Link href="/profile">
-          <Button variant="ghost" size="icon" className="p-[27px]">
-            <ChevronLeft className="size-7 stroke-stone-900" />
-          </Button>
+          <ChevronLeft className="size-7" />
         </Link>
-        <h1 className="text-xl font-semibold text-stone-900">내가 좋아요한 사람</h1>
-        <Button variant="ghost" size="icon" className="p-[27px]">
-          <Siren className="size-7 stroke-stone-900" />
-        </Button>
-      </div>
-    </div>
+      </Button>
+      <h1 className="text-xl font-semibold text-stone-900">내가 좋아요한 사람</h1>
+    </header>
   );
 };
