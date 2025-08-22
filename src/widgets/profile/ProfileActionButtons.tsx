d807aac1a62825d67743cffdc64dbee68f1b9a58
kwonup <kkwon1122@gmail.com>
2025-08-23 02:01:03 +0900

    fix/#118 액션버튼위치조정

--- src/widgets/profile/ProfileActionButtons.tsx
+++ src/widgets/profile/ProfileActionButtons.tsx
@@ -10,7 +10,7 @@ interface ProfileActionButtonsProps {
 
 export const ProfileActionButtons = ({ userId, isLiked }: ProfileActionButtonsProps) => {
   return (
-    <div className="flex flex-col items-center gap-1">
+    <div className="flex flex-col items-center gap-0.5">
       <ProfileLikeButton userId={userId} isLiked={isLiked} />
       <StartChatButton userId={userId} />
     </div>
