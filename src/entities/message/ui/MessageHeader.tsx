be48be30f2dc336ca8b2a09e7ee1828df54ee16b
kwonup <kkwon1122@gmail.com>
2025-08-23 17:51:01 +0900

    fix/카드영역조정,프로필드로워 하단여백,수정페이지 보더 제거

--- src/entities/message/ui/MessageHeader.tsx
+++ src/entities/message/ui/MessageHeader.tsx
@@ -15,7 +15,7 @@ export const MessageHeader = (props: MessageHeaderProps) => {
     <header className="safe-pt relative flex h-(--space-h-header) items-center justify-between bg-white px-4">
       <Button variant="ghost" size="icon" asChild>
         <Link href="/chats">
-          <ChevronLeft className="size-7" />
+          <ChevronLeft className="size-6" />
         </Link>
       </Button>
       <Link href={`/profile/${opponentUserId}`}>
